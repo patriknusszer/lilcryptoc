61bda8d297e9c350564c4cca4be8e6a678f25728
Patrik Nusszer <patrik.nusszer@icloud.com>
2020-07-26 16:52:01 +0200

    Initial Commit

---
+++ lilcryptoc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lilcryptoc
+//
+//  Created by Patrik Nusszer on 2020. 07. 26..
+//  Copyright © 2020. Patrik Nusszer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
